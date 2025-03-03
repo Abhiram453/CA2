b7ca09305357f9fcd1ef1b8acf0817c35d1b45ab
abhiram <abhirammotupalli.141@gmail.com>
2025-03-03 15:55:00 +0530

    all cleared

--- README.md
+++ README.md
@@ -1,1 +1,2 @@
 # CA2
+https://github.com/Abhiram453/CA2.git
--- backend/server.js
+++ backend/server.js
@@ -10,12 +10,13 @@ const PORT=5000
 const db=async()=>{
     try {
         await mongoose.connection(process.env.DB)
-        res.status(200).send('connected')
+        console.log('connected')
     } catch (err) {
         console.log(err.message)
-        res.status(500).send('internal server error')
+    
     }
 }
+db()
 
 app.get('./',(res,req)=>{
     res.send('hello')
@@ -24,8 +25,8 @@ app.post('/movie',async(req,res)=>{
     const {title,director,genre,releaseYear,availableCopies}=req.body
     try {
         const newMovie=new movie(req.body)
-        await movie.save()
-        res.send(200).json(newMovie)
+        await newMovie.save()
+        res.status(200).json(newMovie)
         
     } catch (error) {
         console.log(error)
@@ -38,7 +39,7 @@ app.get('/movie',async(req,res)=>{
     const {title,director,genre,releaseYear,availableCopies}=req.body
     try {
        const movies=await movie.find()
-       res.send(200).json(movies)
+       res.status(200).json(movies)
         
     } catch (error) {
         console.log(error)
@@ -52,7 +53,7 @@ app.put('/movie/:id',async(req,res)=>{
     const {id}=req.params
     try {
        const updateMovie=await movie.findByIdAndUpdate(id,req.body,{new:true})
-       if(!movie){
+       if(!updateMovie){
         res.status(400).send('movie not found')
        }
         res.status(200).send(updateMovie)
@@ -68,7 +69,7 @@ app.delete('/movie/:id',async(req,res)=>{
     const {id}=req.params
     try {
        const deleteMovie=await movie.findByIdAndDelete(id)
-       if(!movie){
+       if(!deleteMovie){
         res.status(400).send('movie not found')
        }
         res.status(200).send(deleteMovie)
